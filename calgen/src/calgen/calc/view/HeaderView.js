ced5643bf82d7f57f2d18efd1d1c0df9ea91cac6
Jack Yin <grossopaforever@gmail.com>
2021-02-10 10:34:03 +0800

    header view to change to material ui components

--- calgen/src/calgen/calc/view/HeaderView.js
+++ calgen/src/calgen/calc/view/HeaderView.js
@@ -1,9 +1,10 @@
+import { FormControlLabel, FormLabel, Grid, MenuItem, Radio, RadioGroup, TextField, Typography } from '@material-ui/core';
+import Select from '@material-ui/core/Select';
 import * as actions from 'calgen/calc/actions';
 import React, { useEffect } from 'react';
-import { Typography, Container, Grid } from '@material-ui/core';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from "react-router-dom";
-import './HeaderView.css'
+import './HeaderView.css';
 
 function OnLoad() {
   const dispatch = useDispatch()
@@ -41,65 +42,69 @@ function HeaderView() {
   }
 
   return (
-    <Container className="noprint" style={{textAlign:'center'}}>
+    <Grid className="noprint" container justify="center">
       <OnLoad />
       <Typography variant="h4" component="h1" align="center">小学数学计算题生成器</Typography>
-      <Grid container direction="column" justify="flex-start" alignItems="center">
-        <table className="options-table">
-          <tbody>
-            <tr>
-              <td>
-                <label>题型</label>
-                <select value={questionType} onChange={event => doUpdate({ questionType: parseInt(event.target.value) })}>
-                  <option value={0}>请选择...</option>
-                  <option value={0x01}>仅加法</option>
-                  <option value={0x10}>仅减法</option>
-                  <option value={0x11}>加减法</option>
-                </select>
-              </td>
-              <td>
-                <label>数字个数</label>
-                <select value={numberCount} onChange={event => doUpdate({ numberCount: parseInt(event.target.value) })}>
-                  <option value={2}>2 (a + b = z)</option>
-                  <option value={3}>3 (a + b + c = z)</option>
-                  <option value={4}>4 (a + b + c + d = z)</option>
-                  <option value={5}>5 (a + b + c + d + e = z)</option>
-                </select>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <label>数值范围</label>
-                <input type="number" min={-100000} max={rangeMax - 1} value={rangeMin}
-                  onChange={event => doUpdate({ rangeMin: parseInt(event.target.value) })} />
-                <span>~</span>
-                <input type="number" min={rangeMin} max={100000} value={rangeMax}
-                  onChange={event => doUpdate({ rangeMax: parseInt(event.target.value) })} />
-              </td>
-              <td>
-                <label>题目数量</label>
-                <input type="number" min={1} max={2000} value={count}
-                  onChange={event => doUpdate({ count: Math.min(2000, parseInt(event.target.value) || 1) })} />
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <label>填空位置</label>
-                <select value={blank} onChange={event => doUpdate({ blank: parseInt(event.target.value) })}>
-                  <option value="1">仅右边</option>
-                  <option value="2">仅左边</option>
-                  <option value="3">两边</option>
-                </select>
-              </td>
-              <td>
-
-              </td>
-            </tr>
-          </tbody>
-        </table>
+      <Grid container spacing={3} justify="center" alignItems="center" style={{ width: '90%' }}>
+        <Grid item xs={2} container direction="row" justify="flex-end">
+          <FormLabel>题型</FormLabel>
+        </Grid>
+        <Grid item xs={4} container direction="row" justify="flex-start">
+          <RadioGroup row defaultValue="1" value={questionType}
+            onChange={event => doUpdate({ questionType: parseInt(event.target.value) })}>
+            <FormControlLabel value={0x01} control={<Radio color="primary" />} label="仅加法" />
+            <FormControlLabel value={0x10} control={<Radio color="primary" />} label="仅减法" />
+            <FormControlLabel value={0x11} control={<Radio color="primary" />} label="加法+减法" />
+          </RadioGroup>
+        </Grid>
+        <Grid item xs={2} container direction="row" justify="flex-end">
+          <FormLabel>数字个数</FormLabel>
+        </Grid>
+        <Grid item xs={4} container direction="row" justify="flex-start">
+          <Select value={numberCount} defaultValue={2}
+            onChange={event => doUpdate({ numberCount: parseInt(event.target.value) })} >
+            <MenuItem value={2}>2 (a + b = z)</MenuItem>
+            <MenuItem value={3}>3 (a + b + c = z)</MenuItem>
+            <MenuItem value={4}>4 (a + b + c + d = z)</MenuItem>
+            <MenuItem value={5}>5 (a + b + c + d + e = z)</MenuItem>
+          </Select>
+        </Grid>
+        <Grid item xs={2} container direction="row" justify="flex-end">
+          <FormLabel>数值范围</FormLabel>
+        </Grid>
+        <Grid item xs={4} container direction="row" justify="flex-start">
+          <TextField type="number" InputLabelProps={{ shrink: true }} inputProps={{ style: { textAlign: 'right' } }} style={{ width: 50 }}
+            min={-100000} max={rangeMax - 1} value={rangeMin}
+            onChange={event => doUpdate({ rangeMin: parseInt(event.target.value) })} />
+          <span className="calc-range-char">~</span>
+          <TextField type="number" InputLabelProps={{ shrink: true }} inputProps={{ style: { textAlign: 'right' } }} style={{ width: 50 }}
+            min={rangeMin} max={100000} value={rangeMax}
+            onChange={event => doUpdate({ rangeMax: parseInt(event.target.value) })} />
+        </Grid>
+        <Grid item xs={2} container direction="row" justify="flex-end">
+          <FormLabel>题目数量</FormLabel>
+        </Grid>
+        <Grid item xs={4} container direction="row" justify="flex-start">
+          <TextField type="number" InputLabelProps={{ shrink: true }} inputProps={{ style: { textAlign: 'right' } }} style={{ width: 50 }}
+            min={1} max={2000} value={count}
+            onChange={event => doUpdate({ count: Math.min(2000, parseInt(event.target.value) || 1) })} />
+        </Grid>
+        <Grid item xs={2} container direction="row" justify="flex-end">
+          <FormLabel>填空位置</FormLabel>
+        </Grid>
+        <Grid item xs={4} container direction="row" justify="flex-start">
+          <RadioGroup row value={blank} defaultValue={1}
+            onChange={event => doUpdate({ blank: parseInt(event.target.value) })}>
+            <FormControlLabel value={1} control={<Radio color="primary" />} label="仅右边" />
+            <FormControlLabel value={2} control={<Radio color="primary" />} label="仅左边" />
+            <FormControlLabel value={3} control={<Radio color="primary" />} label="两边" />
+          </RadioGroup>
+        </Grid>
+        <Grid item xs={12} container justify="center">
+          <button className="gen-button" onClick={() => dispatch(actions.generateQuestions(questionType, rangeMin, rangeMax, numberCount, count, blank))}>生成</button>
+        </Grid>
       </Grid>
-      <button className="gen-button" onClick={() => dispatch(actions.generateQuestions(questionType, rangeMin, rangeMax, numberCount, count, blank))}>生成</button>
-    </Container>
+    </Grid>
   )
 }
 
